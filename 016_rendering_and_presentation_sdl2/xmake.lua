bf2ef5da3568e24a50fd144d4e36709def45609b
Chineseguuys <yjh6102114028@gmail.com>
2024-10-13 23:29:32 +0800

    try render vulkan by using SDL2 window library instead of glfw

---
+++ 016_rendering_and_presentation_sdl2/xmake.lua
@@ -0,0 +1,27 @@
+-- 查找 spdlog 库， 类似于 cmake 中的 find_package() 函数
+add_requires("spdlog", {system = true})
+
+-- global definations
+--add_defines("USE_SELF_DEFINED_CLEAR_COLOR")
+
+-- debug log print
+add_defines("LOG_DEBUG")
+add_defines("USING_SDL2")
+
+if is_mode("debug") then
+    set_symbols("debug")
+    set_optimize("none")
+    add_defines("DEBUG")
+    set_strip("none")
+    add_cxflags("-g")
+    add_ldflags("-g")
+end
+
+target("rendering")
+    set_kind("binary")
+    add_files("main.cpp")
+
+    add_packages("spdlog::spdlog")
+    add_packages("SDL2")
+
+    add_links("glfw", "glad", "pthread", "vulkan", "SDL2")
